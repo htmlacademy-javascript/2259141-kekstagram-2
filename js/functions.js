9f76c777d794c3e4d5d373a9323459e55af48225
Евгения Косьяненко <carminaves@gmail.com>
2024-12-24 20:30:49 +0300

    Нужно больше функций

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,12 @@
 function checkLength (string, length) {
-  if (string.length <= length) {
-    return true;
-  } else {
-    return false;
-  }
+  return string.length <= length;
 }
 function isPalindrome (string) {
   string = string.replaceAll(' ', '');
-  string = string.toApperCase();
+  string = string.toUpperCase();
   let reversedString = '';
   for (let i = string.length - 1; i >= 0; i--) {
     reversedString += string[i];
-    return string === reversedString;
   }
+  return string === reversedString;
 }
